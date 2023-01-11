37de2a352d38067250cb419642e44d0d426c2697
marcos <marcos.millan@gigas.com>
2023-01-11 11:31:18 +0100

    try fix bug 2

--- login/test_login.py
+++ login/test_login.py
@@ -46,7 +46,7 @@ def test_google():
     chrome_options.add_argument('--disable-gpu')'''
     print('aaaaaaaaaaaaaaaaaaa')
     #driver = webdriver.Chrome(service = chrome_service, options=chrome_options) # OK
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service = ChromeService(ChromeDriverManager().install()))
     print('bbbbbbbbbbbbb')
     driver.set_window_position(5000, 18000)
     driver.maximize_window()
